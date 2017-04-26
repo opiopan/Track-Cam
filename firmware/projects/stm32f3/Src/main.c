40dab5d74abe07ba6e69ad4bb3733ea994716fe4
opiopan <opiopan@gmail.com>
2017-04-26 23:12:14 +0900

    fix TIM1 configuration bug

--- firmware/projects/stm32f3/Src/main.c
+++ firmware/projects/stm32f3/Src/main.c
@@ -266,9 +266,9 @@ static void MX_TIM1_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 1500;
+  htim1.Init.Prescaler = 2;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 1000-1;
+  htim1.Init.Period = 1024-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
